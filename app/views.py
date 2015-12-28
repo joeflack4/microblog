aecd378e5b386f990b06bebacf93f34f85bc1b0c
joeflack4 <joeflack4@gmail.com>
2015-12-28 16:49:41 -0600

    Favicon & Index.html

--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    #return "Hello, World!"
+	return render_template('index.html')
+	
+#@app.route('/hello/')
+#@app.route('/hello/<name>')
+#def hello(name=None):
+#    return render_template('hello.html', name=name)
