edc016253f7226488e79e3b2e0eaad3406b3fb1d
joeflack4 <joeflack4@gmail.com>
2016-01-17 04:17:27 -0600

    Added Static File Templates - AdminLTE

--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
-from flask import render_template
+from flask import render_template, url_for
 from app import app
 
 @app.route('/')
-@app.route('/index')
-@app.route('/index/<name>')
-def index(name=None):
+#@app.route('/index')
+#@app.route('/index/<name>')
+def index():
     #How to use in local environment: 
 	#	localhost:5000/index/name
 	
-	return render_template('index.html', name=name)
+	return render_template('/AdminLTE/index.html')
